50ebe294443a53250d9a4915568143fd8af8bb73
Sergiy Moskalenko <sergiyvm96@gmail.com>
2016-01-06 23:34:31 +0200

    server fix

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require("http");
 var url = require("url");
 var port = process.env.PORT || 1337;
 
+start();
+
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
