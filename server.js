e5676175f962f4aa39dca68cf699aed461f83663
Sergiy Moskalenko <sergiyvm96@gmail.com>
2016-01-06 23:43:54 +0200

    test again

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-var http = require("http");
-var url = require("url");
-var port = process.env.PORT || 1337;
+// var http = require("http");
+// var url = require("url");
+// var port = process.env.PORT || 1337;
+//
+// start();
+//
+// function start(route, handle) {
+//   function onRequest(request, response) {
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("Request for " + pathname + " received.");
+//
+//     route(handle, pathname);
+//
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+//   }
+//
+//   http.createServer(onRequest).listen(port);
+//   console.log("Server has started.");
+// }
+//
+// exports.start = start;
 
-start(); //
-
-function start(route, handle) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-
-    route(handle, pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(port);
-  console.log("Server has started.");
-}
-
-exports.start = start;
-
-/*var http = require('http')
+var http = require('http')
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
-*/
